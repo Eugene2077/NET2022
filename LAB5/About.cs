d69a9a7a7437a56d08d3817fc26933830d3007dd
Eugene2077 <99775160+Eugene2077@users.noreply.github.com>
2022-03-27 20:35:48 -0400

    Add files via upload

---
+++ LAB5/About.cs
@@ -0,0 +1,27 @@
+﻿
+/* author : Hongju Shin
+ * ID:  100844220
+ * Date: 27 Mar 2022
+ * Subject: Lab5
+ * Purpose: simple text editor project
+ */
+
+
+using System;
+using System.Windows.Forms;
+
+namespace LAB5
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
