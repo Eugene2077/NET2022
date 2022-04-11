6105d486952a819e2ca53ffad3ce168ed4f6669c
Eugene2077 <99775160+Eugene2077@users.noreply.github.com>
2022-04-11 15:14:55 -0400

    Add files via upload

---
+++ ICE01/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace ICE01
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblQuestion.Location = new System.Drawing.Point(36, 33);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(273, 112);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "In Jan of 2022, the computer has detected that Hongju Shin is awesome.  Is it pos" +
+    "sible that they are awesome?";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.lblQuestion, "In Jan of 2022, the computer has detected that Hongju Shin is awesome.  Is it pos" +
+        "sible that they are awesome?");
+            this.lblQuestion.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // btnYes
+            // 
+            this.btnYes.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnYes.Location = new System.Drawing.Point(15, 200);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(75, 40);
+            this.btnYes.TabIndex = 1;
+            this.btnYes.Text = "&Yes";
+            this.toolTip1.SetToolTip(this.btnYes, "Yes button");
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnNo.Location = new System.Drawing.Point(100, 200);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(75, 40);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "&No";
+            this.toolTip1.SetToolTip(this.btnNo, "No button");
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.MouseHover += new System.EventHandler(this.mouse_hover);
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnYes;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnNo;
+            this.ClientSize = new System.Drawing.Size(344, 261);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.lblQuestion);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Awesomeness - <Hongju Shin>";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ ICE01/Form1.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Name: Hongju(Eugene) Shin
+ * Program: NETD-2202-05
+ * Created on January 20, 2022, 7:22 PM
+ * Description: simple app for practicing windows forms
+*/
+using System;
+using System.Windows.Forms;
+
+namespace ICE01
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yes, Hongju Shin is awesome, aren't they!");
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void mouse_hover(object sender, EventArgs e)
+        {
+
+            var mousecursor = this.PointToClient(Cursor.Position);  // get the mouse position in the app
+    
+            if (mousecursor.X > 100 && mousecursor.X <175)  // if the mouse is the original location of the button
+            {
+                btnNo.Left = btnNo.Left + 100;  // move the button
+        
+            }
+            else                                // if the button is not there
+            { 
+                btnNo.Left = btnNo.Left - 100;  // back to original location
+               
+            }
+            
+        }
+
+    }
+}
